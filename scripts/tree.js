5f1aedd035a0ed9dd67779ccdd19278350949435
Christopher Ferris <christopher.d.ferris@gmail.com>
2020-02-14 14:05:54 -0800

    Installed + started setting up yargs

--- scripts/tree.js
+++ scripts/tree.js
@@ -1,9 +1,26 @@
 const fs = require('fs-extra');
 
 const run = async () => {
+  // const argv = require('yargs')
+  //   .option('input-file', {
+  //     alias: 'i',
+  //     type: 'string',
+  //     description: 'The input file to parse',
+  //     demandOption: true
+  //   })
+  //   .option('output-file', {
+  //     alias: 'o',
+  //     type: 'string',
+  //     description: 'The output file',
+  //     demandOption: true
+  //   })
+  //   .help()
+  //   .argv
+
+  // console.log('argv', argv)
   const { treeifyFile } = require('../src/treeify');
-  const result = await treeifyFile(`${__dirname}/structure.txt`);
-  await fs.writeFile(`${__dirname}/structure-tree.txt`, result);
+  const result = await treeifyFile(`${__dirname}/source.txt`);
+  await fs.writeFile(`${__dirname}/source.txt`, result);
 }
 
-run()
+run();
