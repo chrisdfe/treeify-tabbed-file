ffe03c4ba669f4e800ec7740eb1d82fe5b11e84e
chrisdfe <christopher.d.ferris@gmail.com>
2020-02-14 23:00:22 -0800

    cleanup

--- src/treeify.js
+++ src/treeify.js
@@ -1,18 +1,19 @@
-const fs = require('fs-extra');
-const treeify = require('object-treeify');
-const set = require('lodash.set');
+const fs = require("fs-extra");
+const treeify = require("object-treeify");
+const set = require("lodash.set");
 
 const TAB_SPACING = 2;
-const PATH_SEPEARATOR = '.'
+const PATH_SEPEARATOR = ".";
 
-const isSpaceCharacter = str => str === ' ';
+const isSpaceCharacter = str => str === " ";
 
-const createPathSeperatorRegex = seperator => new RegExp(`/\w\${seperator}\w/`, 'g');
+const createPathSeperatorRegex = seperator =>
+  new RegExp(`/\w\${seperator}\w/`, "g");
 
 const getPathSeparatorCount = path => {
   const seperatorRegex = createPathSeperatorRegex(PATH_SEPEARATOR);
-  return (path.match(seperatorRegex) || []).length
-}
+  return (path.match(seperatorRegex) || []).length;
+};
 
 const getLineStartSpaces = str => {
   let count = 0;
@@ -24,14 +25,13 @@ const getLineStartSpaces = str => {
     }
   }
   return count;
-}
+};
 
 module.exports.getLineStartSpaces = getLineStartSpaces;
 
-
 const rawFormatToObject = str => {
   const paths = [];
-  const lines = str.split('\n');
+  const lines = str.split("\n");
   const currentPath = [];
   let currentTabIndex = -1;
 
@@ -45,47 +45,35 @@ const rawFormatToObject = str => {
       currentPath.pop();
     } else if (tabIndex < currentTabIndex) {
       const amountFewer = currentTabIndex - tabIndex;
-      [...(new Array(amountFewer + 1))].forEach(() => {
+      [...new Array(amountFewer + 1)].forEach(() => {
         currentPath.pop();
-      })
+      });
     }
 
     currentPath.push(trimmedLine);
 
-    // console.log('currentPath', currentPath.join(PATH_SEPEARATOR))
     paths.push(currentPath.join(PATH_SEPEARATOR));
 
     currentTabIndex = tabIndex;
   });
 
-  const sortedPaths = [...paths].sort((pathA, pathB) => {
-    const pathASeperatorCount = getPathSeparatorCount(pathA);
-    const pathBSeperatorCount = getPathSeparatorCount(pathB);
-    return pathASeperatorCount - pathBSeperatorCount;
-  })
-
-  // console.log('sortedPaths', sortedPaths)
-
   const result = {};
-  sortedPaths.forEach(path => {
+  paths.forEach(path => {
     if (path) {
       set(result, path, {});
     }
   });
 
-  // console.log('result', JSON.stringify(result, null, 4))
-
   return result;
-}
+};
 
 module.exports.rawFormatToObject = rawFormatToObject;
 
-
 const treeifyFile = async file => {
-   const contents = await fs.readFile(file);
-   const fileTreeObject = rawFormatToObject(contents.toString());
-   const tree = treeify(fileTreeObject);
-   return tree;
-}
+  const contents = await fs.readFile(file);
+  const fileTreeObject = rawFormatToObject(contents.toString());
+  const tree = treeify(fileTreeObject);
+  return tree;
+};
 
 module.exports.treeifyFile = treeifyFile;
